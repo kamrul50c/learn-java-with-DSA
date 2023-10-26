7876a69d84a874c26bb5c3b6b25535cc35dd222c
kamrul50c <khasan50c03@gmail.com>
2023-10-26 06:41:31 +0600

    basic input output

--- java_basics.java
+++ java_basics.java
@@ -1,6 +1,15 @@
+import java.util.*;                          // import liberary for input
 public class java_basics{
     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);   // scanner object;
 
+        
+      
+
+        String s=sc.nextLine();
+        
+
+        System.out.println("Hello ! "+ s);
         
     }
 }
