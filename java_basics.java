11efd6f5b484efa1d056f3d896d760229eda4627
kamrul50c <khasan50c03@gmail.com>
2023-10-26 16:00:18 +0600

    if else - switch case - ternary

--- java_basics.java
+++ java_basics.java
@@ -18,7 +18,7 @@ public class java_basics{
     // // type casting 
 
     // float c=10.5;           
-    float cal=15.6f *c;        // decimal value automatic consider as double type but using 'f' float 
+   // float cal=15.6f *c;        // decimal value automatic consider as double type but using 'f' float 
     // int d=(int)c;
 
 
@@ -29,6 +29,66 @@ public class java_basics{
 
     //     System.out.println(calculate);
 
+      // operator // arithmetic - //unary -relation - //logical -//assignment
+
+    // conditional statement //if-else // switch case // ternary::
+
+    // find large number using if else:
+
+    int num_1=sc.nextInt();
+    int num_2=sc.nextInt();
+    int num_3=sc.nextInt();
+    int max;
+    if(num_1>num_2 && num_1>num_3){
+
+        max=num_1;
+    }else if(num_2>num_3){
+        max=num_2;
+    }else{
+        max=num_3;
+    }
+
+System.out.println("large number is = "+max);
+
+
+// calculator using switch case 
+
+float result=0;
+char ch=sc.next().charAt(0);
+float a=sc.nextFloat();
+float b=sc.nextFloat();
+
+
+switch(ch){
+
+    case '+':
+      result=a+b;
+     break;
+    case '-':
+      result=(a>b)?a-b:b-a;
+      break;
+    case '*':
+      result=a*b;
+      break;
+    case '/':
+      result=a/b;
+      break;
+    case '%':
+      result=a%b;
+      break;
+    default:
+      System.out.println("wrong input");
+       
+      }
+      System.out.println(result);
+
+
+
+// even odd using ternary operator:
+
+int a=sc.nextInt();
+String st=(a%2==0)?"even":"odd";
+System.out.println(st);
 
   
 
