ca93faabc39a550d333dfe8c55a97cf059cca391
kamrul50c <khasan50c03@gmail.com>
2023-10-26 15:50:15 +0600

    clear some conceft and fixed

--- java_basics.java
+++ java_basics.java
@@ -1,7 +1,7 @@
-import java.util.*;                          // import liberary for input
+import java.util.*;                          // import liberary for Scanner and all
 public class java_basics{
     public static void main(String args[]){
-        // Scanner sc=new Scanner(System.in);   // scanner object;
+        Scanner sc=new Scanner(System.in);   // scanner object;
         // String s=sc.nextLine();
         // System.out.println("Hello ! "+ s);
 
@@ -11,21 +11,26 @@ public class java_basics{
         // byte -1. short - 2. char -2. boolean -1. int -4. float-4. double-8 long - 8:
 
     //type conversion 
-    int a=10;
-    float b=a;
+    // int a=10;
+    // float b=a;
 
 
-    // type casting 
+    // // type casting 
 
-    float c=10.5f;       // decimal value automatic assign as double type but using 'f' float 
-    int d=(int)c;
+    // float c=10.5;           
+    float cal=15.6f *c;        // decimal value automatic consider as double type but using 'f' float 
+    // int d=(int)c;
 
 
-    //type promotion     // automatically promotes small size datatype can be promoted to a large size datatype.
+    // //type promotion     // automatically promotes small size datatype can be promoted to a large size datatype.
      
-    float calculate = (float)a*c;    //type promotion and type casting
+    // float calculate = (float)a*c;    //type promotion and type casting
 
 
-        System.out.println(calculate);
+    //     System.out.println(calculate);
+
+
+  
+
     }
 }
