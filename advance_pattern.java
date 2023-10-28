e126ed191119b74154dcb126d3c5bef269c15c23
kamrul50c <khasan50c03@gmail.com>
2023-10-28 11:20:03 +0600

    butterfly pattern

--- advance_pattern.java
+++ advance_pattern.java
@@ -1,25 +1,47 @@
 import java.util.*;
 public class advance_pattern {
 
-    public static void pattern(int n){
+    public static void butterfly_pattern(int n){
 
-        for(int i=1; i<=n; i++){
-
-            for(int j=1; j<=(n-i); j++){
-
-                System.out.print(" ");
-            }
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
-            }
-
-            System.out.println();
+        // 1st half 
+     for(int i=1; i<=n; i++){
+        //star  i 
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
         }
-    }
+        //star  2*(n-i)
+        for(int j=1; j<=2*(n-i); j++){
+            System.out.print(" ");
+        }
+        //star  i
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println( );
+     } 
 
+     // 2nd half // mirror
+     for(int i=n; i>=1; i--){
+        //star  i 
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        //star  2*(n-i)
+        for(int j=1; j<=2*(n-i); j++){
+            System.out.print(" ");
+        }
+        //star  i
+        
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println( );
+     } 
+       
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int row=sc.nextInt();
-        pattern(row);
+        butterfly_pattern(row);
     }
 }
