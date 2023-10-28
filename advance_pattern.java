92514afce411e00716ed6152318e774d4c843672
kamrul50c <khasan50c03@gmail.com>
2023-10-28 11:43:01 +0600

    hollow rhombus

--- advance_pattern.java
+++ advance_pattern.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class advance_pattern {
 
-    public static void solid_rombus(int n){
+    public static void hollw_rhombus(int n){
 
         // 1st half 
      for(int i=1; i<=n; i++){
@@ -10,7 +10,11 @@ public class advance_pattern {
             System.out.print(" ");
         }
         for(int j=1; j<=n; j++){
-            System.out.print("*");
+            if(j==1 || j==n || i==1 ||i== n){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
         }
   
         
@@ -22,6 +26,6 @@ public class advance_pattern {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int row=sc.nextInt();
-        solid_rombus(row);
+        hollw_rhombus(row);
     }
 }
