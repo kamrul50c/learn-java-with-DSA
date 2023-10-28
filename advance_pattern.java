8d4fae95ce538a40f1898d068645ba722ec9418d
kamrul50c <khasan50c03@gmail.com>
2023-10-28 11:37:07 +0600

    solid rhombus

--- advance_pattern.java
+++ advance_pattern.java
@@ -1,47 +1,27 @@
 import java.util.*;
 public class advance_pattern {
 
-    public static void butterfly_pattern(int n){
+    public static void solid_rombus(int n){
 
         // 1st half 
      for(int i=1; i<=n; i++){
         //star  i 
-        for(int j=1; j<=i; j++){
-            System.out.print("*");
-        }
-        //star  2*(n-i)
-        for(int j=1; j<=2*(n-i); j++){
+        for(int j=1; j<=(n-i); j++){
             System.out.print(" ");
         }
-        //star  i
-        for(int j=1; j<=i; j++){
+        for(int j=1; j<=n; j++){
             System.out.print("*");
         }
-        System.out.println( );
-     } 
-
-     // 2nd half // mirror
-     for(int i=n; i>=1; i--){
-        //star  i 
-        for(int j=1; j<=i; j++){
-            System.out.print("*");
-        }
-        //star  2*(n-i)
-        for(int j=1; j<=2*(n-i); j++){
-            System.out.print(" ");
-        }
-        //star  i
+  
         
-        for(int j=1; j<=i; j++){
-            System.out.print("*");
-        }
         System.out.println( );
-     } 
+    }
+     
        
     }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int row=sc.nextInt();
-        butterfly_pattern(row);
+        solid_rombus(row);
     }
 }
