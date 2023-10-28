658a385d878218d646d50db8f2aa6cc7520636a2
kamrul50c <khasan50c03@gmail.com>
2023-10-28 12:56:04 +0600

    diamond

--- advance_pattern.java
+++ advance_pattern.java
@@ -1,21 +1,38 @@
 import java.util.*;
 public class advance_pattern {
 
-    public static void hollw_rhombus(int n){
+    public static void diamond(int n){
 
         // 1st half 
      for(int i=1; i<=n; i++){
-        //star  i 
+        //spaces  n-i 
         for(int j=1; j<=(n-i); j++){
             System.out.print(" ");
         }
-        for(int j=1; j<=n; j++){
-            if(j==1 || j==n || i==1 ||i== n){
-                System.out.print("*");
-            }else{
-                System.out.print(" ");
+
+        //star odd 2*(i-)1+1
+        for(int j=1; j<=2*i-1; j++){
+            System.out.print("*");
+
             }
+        
+  
+        
+        System.out.println( );
+    }
+    // 2nd half
+     for(int i=n; i>=1; i--){
+        //spaces  n-i 
+        for(int j=1; j<=(n-i); j++){
+            System.out.print(" ");
         }
+
+        //star odd 2*(i-)1+1
+        for(int j=1; j<=2*i-1; j++){
+            System.out.print("*");
+
+            }
+        
   
         
         System.out.println( );
@@ -26,6 +43,6 @@ public class advance_pattern {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int row=sc.nextInt();
-        hollw_rhombus(row);
+        diamond(row);
     }
 }
