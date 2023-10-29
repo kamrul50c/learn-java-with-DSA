c1e3cb7ac50f4519a968fe1bf3d978f2dd1f3b9f
kamrul50c <khasan50c03@gmail.com>
2023-10-29 18:15:49 +0600

    array basic creation input and output

---
+++ array-data-structure/arraybasic.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class arraybasic {
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        int size=sc.nextInt();
+        //array creation //
+       int container[]=new int [size];
+       array_input(container);
+       System.out.println("  ");
+       array_output(container);
+
+
+      
+    }
+
+    public static void array_input(int array[]){
+        Scanner input=new Scanner(System.in);
+
+        for(int i=0; i<array.length; i++){
+            array[i]=input.nextInt();
+
+        }
+    }
+    public static void array_output(int array[]){
+
+        for(int i=0; i<array.length; i++){
+           System.out.print( array[i]+" ");
+
+        }
+    }
+   
+}
