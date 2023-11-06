68dc1c889c351d5b1f753686d703a42ab4ec1b99
kamrul50c <khasan50c03@gmail.com>
2023-11-06 10:32:23 +0600

    counting sort

--- array-data-structure/sort_algorithm.java
+++ array-data-structure/sort_algorithm.java
@@ -5,29 +5,52 @@ public class sort_algorithm {
     public static void main(String args[]){
 
         Scanner sc=new Scanner(System.in);
-        int array[]={5,3,9,2,0,4,6};
+        int array[]={5,2,10,2,0,4,6};
         // call
-        insertion_sort(array);
+        counting_sort(array);
+       // insertion_sort(array);
       //  selection_sort(array);
         // print 
         out(array);
-
-    }
-
-    public static void insertion_sort(int array[]){
-        for(int i=1; i<array.length; i++){
-            int current=array[i];
-            int previous=i-1;
-            // finding the position to insert 
-            while(previous>=0 && array[previous]>current){
-                array[previous+1]=array[previous];
-                previous--;
-            }
-             // insert
-             array[previous+1]=current;
-        }
         
+
     }
+
+    public static void counting_sort(int array[]){
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<array.length; i++){
+            max=Math.max(max,array[i]);
+        }
+        int count_array[]=new int[max+1];
+        for(int i=0; i<array.length; i++){
+            count_array[array[i]]+=1;
+        }
+        int index=0;
+        
+        for(int i=0; i<count_array.length; i++){
+            
+            while(count_array[i]>=1){
+                array[index]=i;
+                count_array[i]--;
+                index++;
+            }
+        }
+    }
+
+    // public static void insertion_sort(int array[]){
+    //     for(int i=1; i<array.length; i++){
+    //         int current=array[i];
+    //         int previous=i-1;
+    //         // finding the position to insert 
+    //         while(previous>=0 && array[previous]>current){
+    //             array[previous+1]=array[previous];
+    //             previous--;
+    //         }
+    //          // insert
+    //          array[previous+1]=current;
+    //     }
+        
+    // }
     // public static void buble_sort(int array[]){
     //     for(int i=0; i<array.length; i++){
     //         for(int j=0; j<array.length-1; j++){
@@ -63,7 +86,6 @@ public class sort_algorithm {
         for(int i=0; i<n; i++){
             System.out.print(array[i]+" ");
         }
-        System.err.println();
-        n--;
+      
     }
 }
