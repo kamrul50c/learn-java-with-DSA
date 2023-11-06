ea719d4dddc33d1c580916fcbdccc744369aec66
kamrul50c <khasan50c03@gmail.com>
2023-11-06 23:35:03 +0600

    2D array transpose matrix

---
+++ array-data-structure/Two_D_array.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Two_D_array {
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int m,n;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        int array[][]=new int [m][n];
+        input(array);
+        //
+      //  out(array);
+
+        //
+        transpose_matrix(array);
+    
+
+
+    }
+
+    public static void input(int array[][]){
+        Scanner input=new Scanner(System.in);
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[0].length;j++){
+                array[i][j]=input.nextInt();
+            }
+            System.out.println();
+        }
+    }
+     public static void out(int array[][]){
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[0].length;j++){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void transpose_matrix(int array[][]){
+        int tr_arr[][]=new int[array[0].length][array.length];
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[0].length; j++){
+                tr_arr[j][i]=array[i][j];
+            }
+        }
+        out(tr_arr);
+        
+    }
+}
