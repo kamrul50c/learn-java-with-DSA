157a33e88db9330773918b096e12769d6c6ddc93
kamrul50c <khasan50c03@gmail.com>
2023-11-11 04:04:57 +0600

    convert first latter to upper

--- string_builder.java
+++ string_builder.java
@@ -2,14 +2,22 @@ public class string_builder {
 
     public static void main(String args[]){
         
-        StringBuilder sb=new StringBuilder("");
-
-        for(char ch='a'; ch<='z'; ch++){
-            sb.append(ch);
-        }
-        System.out.println(sb);
+       
+    String st="hello this is saimon";
+    System.out.println(convert_letter_uppper(st));
     }
     
-    
+    public static String convert_letter_uppper(String st){
+         StringBuilder sb=new StringBuilder("");
+         sb.append(Character.toUpperCase(st.charAt(0)));
+         for(int i=1; i<st.length(); i++){
+            if(st.charAt(i-1) == ' '){
+               sb.append(Character.toUpperCase(st.charAt(i))); 
+            }else{
+                sb.append(st.charAt(i));
+            }
+         }
+      return sb.toString();
+    }
    
 }
